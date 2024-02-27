6edf048eca1cb6319c63b44ac8e210eccd5d7c72
Scott Sickles <scottsickles@gmail.com>
2024-02-27 00:29:50 -0500

    add policy to allow the update of a graphql api

--- access.tf
+++ access.tf
@@ -2,3 +2,28 @@ resource "aws_iam_role_policy_attachment" "access" {
   role       = var.app_metadata["role_name"]
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
 }
+
+resource "aws_iam_role_policy_attachment" "update-api" {
+  role       = var.app_metadata["role_name"]
+  policy_arn = aws_iam_policy.update-api.arn
+}
+
+
+resource "aws_iam_policy" "update-api" {
+  name        = "enable-association"
+  description = "Policy to enable api association with appsync domain"
+  policy      = data.aws_iam_policy_document.update-api.json
+}
+
+data "aws_iam_policy_document" "update-api" {
+  statement {
+    sid    = "UpdateApi"
+    effect = "Allow"
+
+    actions = [
+      "appsync:UpdateGraphqlApi",
+    ]
+
+    resources = ["*"]
+  }
+}
